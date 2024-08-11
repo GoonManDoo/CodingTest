be06ba0179d5c1734feb2d7882f53baa692d1f3f
NooRy <101534916+GoonManDoo@users.noreply.github.com>
2024-08-11 15:11:12 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 2022;
+        
+       
+            answer -= age - 1;
+     
+        
+        return answer;
+    }
+}
